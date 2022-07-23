b74845513ad044780e38ab748dfa33f1d3e35213
Alex Terentev <Ater1337@yandex.ru>
2022-07-23 21:48:36 +0300

    Solving problems from the CodingBat site

--- RealityShowLearning/src/codingBat/warmup1/notStringEx.java
+++ RealityShowLearning/src/codingBat/warmup1/notStringEx.java
@@ -1,5 +1,9 @@
 package codingBat.warmup1;
 
+//Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+// The value of n will be a valid index of a char in the original string
+// (i.e. n will be in the range 0..str.length()-1 inclusive).
+
 public class notStringEx {
     public static void main(String[] args) {
         print(notString("candy"));
