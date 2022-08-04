ca3e17b71ad2129223a70b37ae3c67c42a36d94c
Alex Terentev <Ater1337@yandex.ru>
2022-08-04 10:20:03 +0300

    Problem solving from the CodingBat website in Kotlin language

---
+++ RealityShowLearning/src/codingBat/warmup2/doubleX.kt
@@ -0,0 +1,27 @@
+package codingBat.warmup2
+
+
+//Given a string, return true if the first instance of "x"
+// in the string is immediately followed by another "x".
+
+
+fun main(args: Array<String>) {
+    println(doubleX("axxbb"))
+    println(doubleX("a"))
+    println(doubleX(""))
+    println(doubleX("axxbb"))
+    println(doubleX("xxbb"))
+    println(doubleX("abbbbbbxxbb"))
+    println(doubleX("axaxbb"))
+}
+
+fun doubleX(str: String): Boolean {
+    var findXX = false
+    for (i in 0..str.length - 2) {
+        if (str.substring(i, i + 2).equals("xx")){
+            findXX = true
+            break
+        }
+    }
+    return findXX
+}
